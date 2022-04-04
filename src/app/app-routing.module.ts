8f7601f522bb8b2371e9d7e2e7721d6c1a10d334
Jurado <jaimealberto.jurado@gmail.com>
2022-04-03 20:54:24 -0500

    Base template and styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: 'home', component: UnderConstructionComponent },
+  // { path: 'summary/month', component: MonthSumaryComponent },
+  // { path: 'summary/annual', component: UnderConstructionComponent },
+  // { path: 'settings', component: SettingsComponent },
+  // { path: 'test', component: EmptyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
