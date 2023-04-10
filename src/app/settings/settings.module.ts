a794edd9b7767b1f45b4e372e96574cbd93f4a49
Jaime Jurado <jaimealberto.jurado@gmail.com>
2023-04-10 16:02:06 -0400

    Setting page set under construction

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { SettingsComponent } from './settings.component';
   ],
   imports: [
     CommonModule,
+    SharedModule,
     SettingsRoutingModule
   ]
 })
