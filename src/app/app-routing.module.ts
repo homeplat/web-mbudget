a1b9d81e5d95830757571f1331d927344644065e
Jaime Jurado <jaimealberto.jurado@gmail.com>
2023-04-10 16:02:24 -0400

    Home page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
-  // { path: 'home', component: UnderConstructionComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   // { path: 'summary/month', component: MonthSumaryComponent },
   // { path: 'summary/annual', component: UnderConstructionComponent },
   { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) }
